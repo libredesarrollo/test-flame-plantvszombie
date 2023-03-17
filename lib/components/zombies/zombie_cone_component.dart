231b76691c3ca6de818af699457311b7cc2b20b5
libredesarrollo <desarrollolibre.net@gmail.com>
2023-03-17 16:57:50 -0400

    varios

--- lib/components/zombies/zombie_cone_component.dart
+++ lib/components/zombies/zombie_cone_component.dart
@@ -8,33 +8,33 @@ import 'package:plantsvszombie/components/zombies/zombie_component.dart';
 class ZombieConeComponent extends ZombieComponent {
   ZombieConeComponent({required position}) : super(position) {
     // spriteSheetWidth = 29.2;
-    spriteSheetWidth = 27.6;
+    spriteSheetWidth = 37.4;
     //spriteSheetWidth = 28.6;
-    spriteSheetHeight = 55.5;
+    spriteSheetHeight = 56;
     size = Vector2(spriteSheetWidth, spriteSheetHeight);
     speed = 15;
   }
 
   @override
   Future<void>? onLoad() async {
-    final spriteImage = await Flame.images.load('zombieCone.png');
+    final spriteImage = await Flame.images.load('ZombieConeFinal.png');
 
     final spriteSheet = SpriteSheet(
         image: spriteImage,
         srcSize: Vector2(spriteSheetWidth, spriteSheetHeight));
 
     walkingAnimation = spriteSheet.createAnimationByLimit(
-        xInit: 0, yInit: 5, step: 6, sizeX: 19, stepTime: .2);
+        xInit: 0, yInit: 0, step: 6, sizeX: 12, stepTime: .2);
     eatingAnimation = spriteSheet.createAnimationByLimit(
-        xInit: 0, yInit: 12, step: 6, sizeX: 19, stepTime: .2);
+        xInit: 0, yInit: 7, step: 6, sizeX: 12, stepTime: .2);
     walkingHurtAnimation = spriteSheet.createAnimationByLimit(
-        xInit: 1, yInit: 5, step: 6, sizeX: 19, stepTime: .2);
+        xInit: 1, yInit: 3, step: 5, sizeX: 12, stepTime: .2);
 
     animation = walkingAnimation;
 
     size = Vector2(spriteSheetWidth, spriteSheetHeight);
     body = RectangleHitbox(
-      position: Vector2(0, 0),
+        position: Vector2(0, 0),
         size: Vector2(spriteSheetWidth, spriteSheetHeight - alignZombie))
       ..collisionType = CollisionType.active;
     add(body);
